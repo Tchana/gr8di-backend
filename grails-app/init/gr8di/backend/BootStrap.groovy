95e6aee1fcd6121e24e2700f964713ea228f7b0a
Fosso Arcel <kraulain@gmail.com>
2018-09-11 22:37:34 +0100

    Fosso | bootstrap sample messages for test purposes

--- grails-app/init/gr8di/backend/BootStrap.groovy
+++ grails-app/init/gr8di/backend/BootStrap.groovy
@@ -3,7 +3,10 @@ package gr8di.backend
 class BootStrap {
 
     def init = { servletContext ->
+        new Message(senderEmail:"johnDoe@email.com", topic:"jobs", messageBody:"hello john doe", dateSent: new Date()).save()
+        new Message(senderEmail:"janeDoe@email.com", topic:"internship", messageBody:"hello jane doe", dateSent: new Date()).save()
     }
+
     def destroy = {
     }
 }
