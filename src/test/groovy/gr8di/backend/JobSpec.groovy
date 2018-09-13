93473b8cb0191921905d66dc7fe48a465f34af7c
Tchana valdo <valdotnv@gmail.com>
2018-09-13 22:19:59 +0100

    Valdo,Roy|create job domain class and add fields

---
+++ src/test/groovy/gr8di/backend/JobSpec.groovy
@@ -0,0 +1,18 @@
+package gr8di.backend
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class JobSpec extends Specification implements DomainUnitTest<Job> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
