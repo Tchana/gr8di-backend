69cf593c4fcce9a1888c6e1e641c9805f095d069
Echo2roy <bichiroy50@gmail.com>
2018-09-18 11:28:20 +0100

    Roy, Valdo| create section domain class

---
+++ src/test/groovy/gr8di/backend/SectionSpec.groovy
@@ -0,0 +1,18 @@
+package gr8di.backend
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class SectionSpec extends Specification implements DomainUnitTest<Section> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
