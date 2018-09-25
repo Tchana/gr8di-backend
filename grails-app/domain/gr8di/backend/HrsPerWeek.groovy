303caf4a64a292be5a3827ae39e8550c98fb0d39
Echo2roy <bichiroy50@gmail.com>
2018-09-25 16:09:56 +0100

    Roy, Valdo| Implement Mentor endpoint

---
+++ grails-app/domain/gr8di/backend/HrsPerWeek.groovy
@@ -0,0 +1,12 @@
+package gr8di.backend
+
+import javax.print.attribute.standard.MediaSize
+
+enum HrsPerWeek {
+    lessThanOne,
+    OneToThree,
+    ThreeToFive,
+    FourToSix,
+    SevenToTen,
+    Other
+}
---
+++ grails-app/domain/gr8di/backend/Mentor.groovy
@@ -0,0 +1,19 @@
+package gr8di.backend
+
+class Mentor {
+    String mentorName
+    String timeZone
+    String workDays
+    String menteeAgeRange
+    String specialSkills
+    GroovyExp groovyExp
+    LibFrmwrkTools libFrmwrkTools
+    Language language
+    HrsPerWeek hrsPerWeek
+    TimeOfStart timeOfStart
+
+
+
+    static constraints = {
+    }
+}
