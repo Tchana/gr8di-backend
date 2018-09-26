70e5e38c75236178210b01748a27be031c5d9a71
Echo2roy <bichiroy50@gmail.com>
2018-09-26 10:47:42 +0100

    Roy, Valdo| Create Mentee domain and support classes

---
+++ grails-app/domain/gr8di/backend/Mentee.groovy
@@ -0,0 +1,20 @@
+package gr8di.backend
+
+class Mentee {
+    String menteeName
+    String timeZone
+    String workDays
+    String specialSkills
+    GroovyExp groovyExp
+    LibFrmwrkTools libFrmwrkTools
+    Language language
+    HrsPerWeek hrsPerWeek
+    TimeOfStart timeOfStart
+    Goal goal
+
+
+
+
+    static constraints = {
+    }
+}
